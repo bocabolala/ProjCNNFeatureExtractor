99b87cbb8f8072e4523ac8e191f6eb21dd9afe8a
bocabolala <geekkoala7@gmail.com>
2021-12-15 11:09:25 +0100

    add COCO dataset creator to repo

--- module_test.py
+++ module_test.py
@@ -15,7 +15,7 @@ from pycococreatortools import pycococreatortools
 
 ROOT_DIR = './data/MuLV/'
 IMAGE_DIR = os.path.join(ROOT_DIR, "gray")
-ANNOTATION_DIR = os.path.join(ROOT_DIR, "annotations")
+ANNOTATION_DIR = './cell_data/train/annotations'
  
 INFO = {
     "description": "Segmentation Dataset",
@@ -23,7 +23,7 @@ INFO = {
     "version": "0.1.0",
     "year": 2021,
     "contributor": "Peiyu Lu",
-    "date_created": "Nov.11.2021, (modified on: Dec.2.2021)"
+    "date_created": datetime.datetime.utcnow().isoformat(' ')
 }
  
 LICENSES = [
@@ -33,23 +33,30 @@ LICENSES = [
         "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/"
     }
 ]
- 
+
+# Add new particles in CATEGORIES
 CATEGORIES = [
     {
         'id': 1,
-        'name': 'particles_A',
+        'name': 'obj_1',
         'supercategory': 'particles',
     },
     {
         'id': 2,
-        'name': 'particles_B',
+        'name': 'obj_2',
         'supercategory': 'particles',
-    }
+    },
+    # {
+    #     'id': 3,
+    #     'name': 'obj_3',
+    #     'supercategory': 'particles',
+    # },
+
 
 ]
  
  
-def filter_for_img(root, files):
+def filter_for_jpeg(root, files):
     file_types = ['*.jpeg', '*.jpg', '*.png']
     file_types = r'|'.join([fnmatch.translate(x) for x in file_types])
     files = [os.path.join(root, f) for f in files]
@@ -68,57 +75,58 @@ def filter_for_annotations(root, files, image_filename):
     return files
  
  
-# def main():
-coco_output = {
-    "info": INFO,
-    "licenses": LICENSES,
-    "categories": CATEGORIES,
-    "images": [],
-    "annotations": []
-}
-
-image_id = 1
-segmentation_id = 1
-
-# filter for jpeg images
-for root, _, files in os.walk(IMAGE_DIR):
-    image_files = filter_for_img(root, files)
-    print(root, 'ssdd', files)
-    # go through each image
-    # for image_filename in image_files:
-    #     image = Image.open(image_filename)
-    #     image_info = pycococreatortools.create_image_info(
-    #             image_id, os.path.basename(image_filename), image.size)
-    #     coco_output["images"].append(image_info)
-
-#         # filter for associated png annotations
-#         for root, _, files in os.walk(ANNOTATION_DIR):
-#             annotation_files = filter_for_annotations(root, files, image_filename)
-
-#             # go through each associated annotation
-#             for annotation_filename in annotation_files:
-
-#                 print(annotation_filename)
-#                 class_id = [x['id'] for x in CATEGORIES if x['name'] in annotation_filename][0]
-
-#                 category_info = {'id': class_id, 'is_crowd': 'crowd' in image_filename}
-#                 binary_mask = np.asarray(Image.open(annotation_filename)
-#                                             .convert('1')).astype(np.uint8)
-
-#                 annotation_info = pycococreatortools.create_annotation_info(
-#                         segmentation_id, image_id, category_info, binary_mask,
-#                         image.size, tolerance=2)
-
-#                 if annotation_info is not None:
-#                     coco_output["annotations"].append(annotation_info)
-
-#                 segmentation_id =+ 1
-
-#         image_id =+ 1
-
-# with open('{}/Vironova_Cell_Seg_2021.json'.format(ROOT_DIR), 'w') as output_json_file:
-#     json.dump(coco_output, output_json_file)
+def main():
+    coco_output = {
+        "info": INFO,
+        "licenses": LICENSES,
+        "categories": CATEGORIES,
+        "images": [],
+        "annotations": []
+    }
  
+    image_id = 1
+    segmentation_id = 1
  
-# if __name__ == "__main__":
-#     main()
+    # filter for jpeg images
+    for root, _, files in os.walk(IMAGE_DIR):
+        image_files = filter_for_jpeg(root, files)
+ 
+        # go through each image
+        for image_filename in image_files:
+            image = Image.open(image_filename)
+            image_info = pycococreatortools.create_image_info(
+                    image_id, os.path.basename(image_filename), image.size)
+            coco_output["images"].append(image_info)
+ 
+            # filter for associated png annotations
+            for root, _, files in os.walk(ANNOTATION_DIR):
+                annotation_files = filter_for_annotations(root, files, image_filename)
+ 
+                # go through each associated annotation
+                for annotation_filename in annotation_files:
+ 
+                    print(annotation_filename)
+                    class_id = [x['id'] for x in CATEGORIES if x['name'] in annotation_filename][0]
+ 
+                    category_info = {'id': class_id, 'is_crowd': 'crowd' in image_filename}
+                    binary_mask = np.asarray(Image.open(annotation_filename)
+                                             .convert('1')).astype(np.uint8)
+ 
+                    annotation_info = pycococreatortools.create_annotation_info(
+                            segmentation_id, image_id, category_info, binary_mask,
+                            image.size, tolerance=2)
+ 
+                    if annotation_info is not None:
+                        coco_output["annotations"].append(annotation_info)
+ 
+                    segmentation_id =+ 1
+ 
+            image_id =+ 1
+ 
+    with open('{}/Vironova_Cell_Seg_2021.json'.format(ROOT_DIR), 'w') as output_json_file:
+        json.dump(coco_output, output_json_file)
+ 
+
+ 
+if __name__ == "__main__":
+    main()
